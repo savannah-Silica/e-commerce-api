275f0ed171885d13a5135695c5a02f65095fb564
kipkirui koech <106087508+kipkirui88@users.noreply.github.com>
2023-12-05 22:19:00 +0300

    added models

--- ecommerce_api/products/models.py
+++ ecommerce_api/products/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    quantity_in_stock = models.PositiveIntegerField()
+
+    def __str__(self):
+        return self.name
