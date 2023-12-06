732f0a1754414271816087ebbacf0ab9d011a430
mpiers110 <mpiers110@gmail.com>
2023-12-06 13:29:44 +0300

    create Product model

--- ecommerce_api/products/models.py
+++ ecommerce_api/products/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    quantity_in_stock = models.FloatField()
+    image = models.ImageField(upload_to='product_images/', null=True, blank=True)
