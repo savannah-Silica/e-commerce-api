6c5d259d83564ec9b411cc6298e4ecc8fe0c7f0d
mpiers110 <mpiers110@gmail.com>
2023-12-06 15:27:40 +0300

    Serve media files during development

--- ecommerce_api/ecommerce_api/urls.py
+++ ecommerce_api/ecommerce_api/urls.py
@@ -16,10 +16,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('products/', include('products.urls')),
+    path("admin/", admin.site.urls),
+    path("products/", include("products.urls")),
 ]
 
+# Serve media files during development
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
