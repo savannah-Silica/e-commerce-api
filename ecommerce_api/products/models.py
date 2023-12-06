409af66a4dccbce7a462cd7cba3ecf23af95879d
mpiers110 <mpiers110@gmail.com>
2023-12-06 14:13:19 +0300

    Alter field quantity_in_stock on product

--- ecommerce_api/products/models.py
+++ ecommerce_api/products/models.py
@@ -5,5 +5,5 @@ class Product(models.Model):
     name = models.CharField(max_length=255)
     description = models.TextField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
-    quantity_in_stock = models.FloatField()
+    quantity_in_stock = models.IntegerField()
     image = models.ImageField(upload_to='product_images/', null=True, blank=True)
