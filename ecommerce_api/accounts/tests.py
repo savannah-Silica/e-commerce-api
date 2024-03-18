4345ef986571cdd8c3d5dda185a94af9ea52226a
zablon <zablonoigo51@gmail.com>
2024-03-18 07:38:56 +0300

    CustomUser model test

--- ecommerce_api/accounts/tests.py
+++ ecommerce_api/accounts/tests.py
@@ -1,28 +1,59 @@
 from django.test import TestCase
 from django.contrib.auth import get_user_model
-from django.urls import reverse
 
 
-class SignupPageTest(TestCase):
-    def test_url_exist_at_correct_locaion_signupview(self):
-        response = self.client.get('/accounts/signup/')
-        self.assertEqual(response.status_code, 200)
+
+class UsersManagersTests(TestCase):
+
+    def test_create_user(self):
+            User=get_user_model()
+            user=User.objects.create_user(
+                email="test@user.com",
+                password="secret"
+            )
+
+            self.assertEqual(user.email, "test@user.com")
+            self.assertTrue(user.is_active)
+            self.assertFalse(user.is_staff)
+            self.assertFalse(user.is_superuser)
+
+            try:
+                  self.assertIsNone(user.username)
+            
+            except AttributeError:
+                  pass
+            
+            with self.assertRaises(TypeError):
+                  User.objects.create_user()
+            
+            with self.assertRaises(TypeError):
+                  User.objects.create_user(email="")
+            
+            with self.assertRaises(ValueError):
+                  User.objects.create_user(email="", password="secret")
+
     
-    def test_signup_view_name(self):
-        response = self.client.get(reverse('signup'))
-        self.assertEqual(response.status_code, 200)
-        self.assertTemplateUsed(response, 'accounts/signup.html')
+    def test_create_superuser(self):
+          User=get_user_model()
+          admin_user=User.objects.create_superuser(
+                email="superuser@test.com",
+                password="secret"
+          )
+          self.assertEqual(admin_user.email,"superuser@test.com")
+          self.assertTrue(admin_user.is_active)
+          self.assertTrue(admin_user.is_staff)
+          self.assertTrue(admin_user.is_superuser)
 
-    def test_signup_form(self):
-        response = self.client.post(reverse("signup"), {
-            'name': 'testuser',
-            'email': 'test@mail.com',
-            'address': 'test address',
-            'phone_number': '1234567890',
-            'password1': 'testpass123',
-            'password2': 'testpass123', 
-        })
-        self.assertEqual(response.status_code, 302)
-        self.assertEqual(get_user_model().objects.all().count(), 1)
-        self.assertEqual(get_user_model().objects.all()[0].name, 'testuser')
-        self.assertEqual(get_user_model().objects.all()[0].email, 'test@mail.com')
+          try:
+                self.assertIsNone(admin_user.username)
+         
+          except AttributeError:
+                pass
+          
+          with self.assertRaises(ValueError):
+                User.objects.create_superuser(
+                      email="superuser@test.com",
+                      password="secret",
+                      is_superuser=False
+                )
+
