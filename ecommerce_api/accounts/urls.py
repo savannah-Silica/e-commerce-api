5f39e883d1b53e7f4d8c859a6360c0f2898177fa
zablon <zablonoigo51@gmail.com>
2024-03-18 14:26:27 +0300

    replaced urls with routers

--- ecommerce_api/accounts/urls.py
+++ ecommerce_api/accounts/urls.py
@@ -1,8 +1,21 @@
 from django.urls import path
-from .views import (sign_in,sign_out,sign_up)
+from rest_framework import routers
+from .viewsets import CustomUserViewsets,RegisterViewSet,LoginViewSet,RefreshViewset
+# from .views import (sign_in,sign_out,sign_up)
 
-urlpatterns = [
-    path('login/', sign_in, name='login'),
-    path('logout/', sign_out, name='logout'),
-    path('signup/', sign_up, name='signup'),
+# urlpatterns = [
+#     path('login/', sign_in, name='login'),
+#     path('logout/', sign_out, name='logout'),
+#     path('signup/', sign_up, name='signup'),
+# ]
+
+router=routers.SimpleRouter()
+
+router.register(r'user', CustomUserViewsets, basename='user')
+router.register(r'register', RegisterViewSet, basename='register')
+router.register(r'login', LoginViewSet, basename='login')
+router.register(r'refresh', RefreshViewset, basename='auth-refresh')
+
+urlpatterns=[
+    *router.urls
 ]
