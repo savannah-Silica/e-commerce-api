dea5f5570cc6d774f550a580eb196b1e8b6642c3
zablon <zablonoigo51@gmail.com>
2024-03-18 07:39:53 +0300

    Adding media path

--- ecommerce_api/ecommerce_api/settings.py
+++ ecommerce_api/ecommerce_api/settings.py
@@ -121,8 +121,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = "static/"
-
-MEDIA_URL = ""
+STATICFILES_DIRS=[BASE_DIR,"static"]
+MEDIA_URL = "media/"
 MEDIA_ROOT = BASE_DIR / "static"
 
 
